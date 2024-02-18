e679c37561a73b6a71bf6a0b4db5435246463260
moganesan <developer.moganesan@gmail.com>
2024-02-18 23:49:44 +0530

    hero section added in the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
-import Image from "next/image";
-import { Cairo } from "next/font/google";
-
-const font = Cairo({ subsets: ["latin"] });
+import HeroSection from "@/components/heroSection";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${font.className}`}
-    ></main>
+    <div>
+      <HeroSection />
+    </div>
   );
 }
