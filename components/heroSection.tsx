3c0f43dbdb06026478d35a3449cbf24ccd36167d
moganesan <developer.moganesan@gmail.com>
2024-02-19 23:42:37 +0530

    hero section added

---
+++ components/heroSection.tsx
@@ -0,0 +1,11 @@
+const HeroSection = () => {
+  return (
+    <div>
+      <h1 className="text-center font-bold text-4xl">
+        Web3 Payment Infrastructure for Businesses
+      </h1>
+    </div>
+  );
+};
+
+export default HeroSection;
